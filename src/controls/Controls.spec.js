aaff9838855bd0489afad2d29bc81e4c3c02ce11
Jojo Zhang <jojozhang0327@gmail.com>
2019-09-16 20:55:35 -0400

    complete MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,55 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, getByText } from '@testing-library/react';
+
+import Controls from './Controls'
+
+test('renders without crashing', ()=> {
+  render (<Controls />);
+})
+
+test('unlocked and open', ()=>{
+  const toggleClosed = jest.fn();
+  const { getByText } = render(<Controls locked = {false} closed ={false} toggleClosed={toggleClosed}/>);
+
+  const lockBtn = getByText(/lock gate/i);
+  const closeBtn = getByText(/close gate/i);
+
+  expect(lockBtn.disabled).toBe(true);
+  expect(closeBtn.disabled).toBe(false);
+
+  fireEvent.click(closeBtn);
+  expect(toggleClosed).toBeCalled();
+})
+
+test('unlocked and close', ()=>{
+  const toggleOpen = jest.fn();
+  const toggleLock = jest.fn();
+  const { getByText } = render(<Controls locked={false} closed={true} toggleClosed={toggleOpen} toggleLocked={toggleLock}/>)
+
+  const lockBtn = getByText(/lock gate/i);
+  const openBtn = getByText(/open gate/i);
+
+  expect(lockBtn.disabled).toBe(false);
+  expect(openBtn.disabled).toBe(false);
+
+  fireEvent.click(lockBtn);
+  expect(toggleLock).toBeCalled();
+
+  fireEvent.click(openBtn);
+  expect(toggleOpen).toBeCalled();
+})
+
+test('locked and close', ()=> {
+  const toggleUnLock = jest.fn();
+  const { getByText } = render(<Controls locked={true} closed={true} toggleLocked={toggleUnLock} />)
+
+  const unlockBtn = getByText(/unlock gate/i);
+  const openBtn = getByText(/open gate/i);
+
+  expect(unlockBtn.disabled).toBe(false);
+  expect(openBtn.disabled).toBe(true);
+
+  fireEvent.click(unlockBtn);
+  expect(toggleUnLock).toBeCalled();
+})
